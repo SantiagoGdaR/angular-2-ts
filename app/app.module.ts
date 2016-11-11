e58af5f97f4b960ba2209ca61b8f22e5581c95c8
santiago garcia da rosa <sanganja@gmail.com>
2016-11-11 18:27:20 -0300

    adding bootstrap, routing, navigation, home component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
+
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: 'home',
+                component: HomeComponent
+            },            
+            { 
+                path: '', 
+                component: HomeComponent 
+            }
+        ])
+    ],
+    declarations: [
+        AppComponent, 
+        HomeComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule{
---
+++ app/home/home.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'app/home/home.component.html'
+})
+export class HomeComponent { }
