5eb555182fbd246fdd77c54ba08c4ee998d31045
SantiagoGdaR <sanganja@gmail.com>
2016-11-24 21:59:14 -0300

    Traying differents bindings

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -6,6 +6,34 @@ import { Home } from './home';
   templateUrl: 'app/home/home.component.html'
 })
 export class HomeComponent {
-    home = new Home("We\'re using interpolation in the title!", 
-      "This is the description and we are going to show it in a two way binding!");
+    private _homeDefaultText: string = "This is the description and we are going to show it in a two way binding!";
+    private _homeBtnDefaultText: string = "Event Binding!";
+
+    home = new Home("We\'re using interpolation in the title!", this._homeDefaultText);
+    homeDefaultDescriptionChanged: boolean = false;
+    homeBtnText: string = this._homeBtnDefaultText;
+    changeFontColor: boolean = false;
+    disableInput: boolean = false;
+
+
+
+    onBtnEventBindingClick(){
+      this.homeDefaultDescriptionChanged = !this.homeDefaultDescriptionChanged;
+      this.home.description = this.homeDefaultDescriptionChanged ? "description changed by click event!" : this._homeDefaultText;
+      this.homeBtnText = this.homeDefaultDescriptionChanged ? "Reset Text!" : this._homeBtnDefaultText;
+    }
+
+    onModelDescriptionChange(newValue){
+      this.homeDefaultDescriptionChanged = true;
+      this.homeBtnText = "Reset Text!";
+      this.home.description = newValue;
+    }
+
+    onChangeFontColorClick(){
+      this.changeFontColor = !this.changeFontColor;
+    }
+
+    onDisableBothInputsClick(){
+      this.disableInput =  !this.disableInput;
+    }
 }
